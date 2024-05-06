29e176354c5555d27271ac67457bf3ac58028bbd
AshleyRamer <ashleyramer7@gmail.com>
2024-05-06 19:33:56 +0000

    idfk man

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-import sys
-import hidden_4 as hidden
-
-if __name__ != "__main__":
-    exit()
-
-for name in dir(hidden):
-    if name[0:2] != "__":
-        print(name)
+import hidden_4
+if __name__ == '__main__':
+    for i in range(0, len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] != '_' or dir(hidden_4)[i][1] != '_':
+            print(dir(hidden_4)[i])
