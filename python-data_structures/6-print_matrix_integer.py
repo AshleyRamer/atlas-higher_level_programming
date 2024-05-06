95783832507eded676237a43d888873fa7e532b0
AshleyRamer <ashleyramer7@gmail.com>
2024-05-06 20:45:07 +0000

    living in the matrix is wild

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for column in matrix:
+        for num in column:
+            print("{:d}".format(num), end='')
+            if num != column[-1]:
+                print(" ", end='')
+
+        print("")
