54c6e0788663939f83eff87a61ad60617f990705
AshleyRamer <ashleyramer7@gmail.com>
2024-05-14 17:54:19 +0000

    frick

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -83,14 +83,3 @@ class Rectangle():
         if self.width == 0 or self.height == 0:
             return 0
         return ((2 * self.width) + (2 * self.height))
-
-    def bigger_or_equal(rect_1, rect_2):
-        """ returns biggest rectangle based on area """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_2.area() > rect_1.area():
-            return rect_2
-        else:
-            return rect_1
