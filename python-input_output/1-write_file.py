87147e6c6e84cd66e3e75c2e8a0f2d1ac9543c29
AshleyRamer <ashleyramer7@gmail.com>
2024-05-20 18:32:03 +0000

    again with the blank lines...

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,11 +2,8 @@
 """ 1-write_file """
 
 
-
 def write_file(filename="", text=""):
-    """
-    Function that writes a string to a text file
-    and returns the number of characters written.
-    """
+    """ Function that writes a string to a text file
+    and returns the number of characters written. """
     with open(filename, mode='w', encoding="utf-8") as f:
         return f.write(text)
