c583999423614cf2a6aae4c24429a24f93701a4c
AshleyRamer <ashleyramer7@gmail.com>
2024-05-20 16:56:07 +0000

    test files with cooresponding files

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,8 @@
-test
+#!/usr/bin/python3
+"""returns the list of available attributes and methods"""
+
+
+def lookup(obj):
+    """returns the attributes and methods of obj"""
+
+    return dir(obj)
