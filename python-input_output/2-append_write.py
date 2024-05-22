d0ca72b4ace540ee7405a1d615ef0dd68463e0aa
Ashley Ramer <155585339+AshleyRamer@users.noreply.github.com>
2024-05-22 13:40:50 -0500

    append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""2-append_write"""
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file and returns the number of chars created"""
+with open(filename, mode='a', encoding="utf-8") as f:
+    return f.write(text)
