4e3f7cdb96488298e63e9e4e86d4bd3fa40ebf30
AshleyRamer <ashleyramer7@gmail.com>
2024-05-24 18:54:19 +0000

    push tests in base file

---
+++ python-almost_a_circle/tests/tests_base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""test module for Base class"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Test Base"""
+
+    def test_base_id(self):
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b2.id, 1)
+        self.assertEqual(b3.id, 2)
+
+    def test_base_id_type(self):
+        b = Base(500)
+        b1= Base()
+        self.assertIsInstance(b.id, int)
+        self.assertIsInstance(b1.id, int)
+
+    def test_base_id_type(self):
+        b = Base("")
+        self.assertNotEqual(b.id, "")
+
+    def test_base_id_type(self):
+        b = Base("hello")
+        b1 = Base("goodbye")
+        self.assertEqual(b.id, "hello")
+        self.assertEqual(b1.id, "goodbye")
+    
