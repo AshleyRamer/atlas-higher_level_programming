53be1c77708245e49467366b4081466db0bea8ab
AshleyRamer <ashleyramer7@gmail.com>
2024-06-18 17:02:17 +0000

    I'm not an idiot, but I can't get the checks?!?!

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = '{}' \
-             ORDER BY id ASC".format(argv[4])
-    cur.execute(query)
+    arg_list = argv[4].split()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
+                 ORDER BY id ASC".format(arg_list[0]))
     rows = cur.fetchall()
 
     for row in rows:
