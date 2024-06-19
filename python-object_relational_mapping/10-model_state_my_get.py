e48a03dc784765483f6e224414627489708e2916
AshleyRamer <ashleyramer7@gmail.com>
2024-06-19 20:33:12 +0000

    removed whitespace at last lines of 9,10,13,&14

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
         print("Not found")
     else:
         print(result.id)
-    
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -18,4 +18,3 @@ class City(Base):
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'),
                       nullable=False)
-        
