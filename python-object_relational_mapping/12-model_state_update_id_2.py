d90c4eb65ef7060057825a731d8edf893280a3b0
AshleyRamer <ashleyramer7@gmail.com>
2024-06-19 20:56:56 +0000

    took out '\' in line 10 and 17

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,14 +7,14 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <db_name>".\
+        print("Usage: {} <mysql_username> <mysql_password> <db_name>".
               format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(username, password, db_name))
     Session = sessionmaker(bind=engine)
     session = Session()
