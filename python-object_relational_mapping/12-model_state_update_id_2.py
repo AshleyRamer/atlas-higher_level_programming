737e20c1fa535a36b6984dacd98e01cd6155c181
AshleyRamer <ashleyramer7@gmail.com>
2024-06-19 20:47:21 +0000

    took out for loop and added if/else statement

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,12 +7,16 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     my_url = "mysql+mysqldb://{}:{}@localhost/{}"
-    engine = create_engine(my_url.format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
+                           format(username, password, db_name))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in session.query(State).filter(State.name.ilike('%a%')):
-        session.delete(instance)
-    session.commit()
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+        print("State name updated successfully!")
+    else:
+        print("State with id 2 not found.")
+    session.close()
