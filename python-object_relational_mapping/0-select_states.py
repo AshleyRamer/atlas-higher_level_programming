23b5afbd9fd515360a536f00c9be9c82ea72d804
AshleyRamer <ashleyramer7@gmail.com>
2024-06-19 20:03:55 +0000

    added sys to args

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ import sys
 
 if __name__ == "__main__":
     """ code for script """
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
